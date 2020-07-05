301195341d19bf4e44f235a6b9ae5e6ce52a1d2c
mwicaksono <56952970+mwicaksono@users.noreply.github.com>
2020-07-05 08:48:50 +0700

    Update windows_startup_script.py

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, events
-API_KEY="Type that here"
-API_HASH="Type that here"
+API_KEY="1124443"
+API_HASH="4a5038cc4c4ef047eb78ba1bbf4495f4"
 #get it from my.telegram.org
 bot = TelegramClient('userbot',API_KEY,API_HASH)
 bot.start()
